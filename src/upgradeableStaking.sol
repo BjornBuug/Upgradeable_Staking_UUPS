e047e4a262f1fe257a55adcf090c8f06bfe0824b
Tayarthouail <tayarthouail@gmail.com>
2023-09-07 17:56:31 +0200

    add functions to handle rewards calculations

--- src/upgradeableStaking.sol
+++ src/upgradeableStaking.sol
@@ -24,7 +24,8 @@ import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable
                   1- Keep track of all staked tokens. [x]
                   2- Keep track of all stakers deposits & withdraw, to do so: we need to create a mapping from msg.sender => struct
                   The struct will contain the stakers info to update [x]
-                  - create functions
+                  - create a function to updateRewards() & claimRewards [x]
+                  - To do: Override pause and unpause functions +  Getters(getPendingRewards function + getStaker Info function)
 
  */
 
@@ -43,17 +44,23 @@ contract UpgradeableStaking is Initializable,
 
 
     //***************************** ERROR ************/
-    error cannotBeZero();
+    error ERR_CANNOT_BE_ZERO();
+    error ERR_CANNOT_UPDATE_REWARDS_YET();
+
 
 
     //***************************** EVENTS ************/
     event Staked(address indexed staker,
                  uint256 indexed amount,
-                 uint256 indexed timestamp);
+                 uint256 indexed timeStamp); // What is indexed and why it used for.
 
     event Unstaked(address indexed staker,
                  uint256 indexed amount,
-                 uint256 indexed timestamp);
+                 uint256 indexed timeStamp);
+
+    event Claimed(address indexed claimAmt,
+                  uint256 indexed amount,
+                  uint256 indexed timeStamp;             
 
 
     uint256 public totalStakedTokens;
@@ -61,11 +68,19 @@ contract UpgradeableStaking is Initializable,
     // Keep track of all stakers Info
     struct StakerInfo {
         uint256 amountStaked;
-        uint256 LastTimestamp; // Last time the staker stakes tokens.
+        uint256 lastTimestamp; // Last time the staker stakes tokens.
         uint256 DebtRewards;
     }
 
     mapping (address => StakerInfo) public stakers;
+    
+    uint256 public rewardsPerToken;
+    uint256 public rewardsEndTime;
+    uint256 public rewardsStartTime;
+    uint256 public lastUpdateTime;
+    uint256 public rewardsRate;
+    
+    mapping (address => uint256) public userRewardsPerTokensPaid;
     
 
 
@@ -85,22 +100,24 @@ contract UpgradeableStaking is Initializable,
     }
 
 
+
+
     /**
      * @dev Function to stake tokens
      * @param _amount amount of stake tokens
      * 
      */
-     function stake(uint256 _amount) external {
+     function stake(uint256 _amount) external whenNotPaused {
         if(_amount <= 0) {
-            revert cannotBeZero();
+            revert ERR_CANNOT_BE_ZERO();
         }
 
         StakerInfo storage staker = stakers[msg.sender];
+        
+        rewardsHandler(staker);
 
         staker.amountStaked += _amount;
         totalStakedTokens += _amount;
-
-        // Handle rewards function
 
         token.safeTransferFrom(msg.sender, address(this), _amount);
 
@@ -108,89 +125,124 @@ contract UpgradeableStaking is Initializable,
 
     }
     
-}
+   }
 
 
+   /**
+     * @dev Function to stake tokens
+     * @param _amount amount of stake tokens
+     * 
+     */
+     // @audit-issue check if the caller is the staker
+     function UnstakeAndClaim(uint256 _amount) external whenNotPaused {
+        if(_amount <= 0) {
+            revert ERR_CANNOT_BE_ZERO();
+        }
+
+        StakerInfo storage staker = stakers[msg.sender];
+        uint256 amountStaked = staker.amountStaked;
+        uint256 claimedRewards = staker.rewardDebt;
+
+        rewardsHandler(staker);
+
+        amountStaked-= _amount;
+        totalStakedTokens -= _amount;
+
+        token.safeTransfer(msg.sender, _amount);
+
+        // ** Claim rewards **/
+         claimedRewards = 0; 
+        _mint(msg.sender, claimedRewards);// Let check the security aspect of including Claim in this function.
+
+        emit Unstaked(msg.sender, _amount, block.timestamp);
+        emit Claimed(msg.sender, claimedRewards, block.timestamp);
+
+    }
+    
+
+   ///@notice function that allow to update rewards rate & rewardsEndTime & rewardsstarttime
+   function updateRewards(uint256 _amount, uint256 duration) external onlyOwner {
+        
+        // Check if the contract isn't giving rewards any more to be able to start a new update;
+        if(rewardsEndTime > block.timestamp) {
+            revert ERR_CANNOT_UPDATE_REWARDS_YET();
+        }
+
+        if(_amount <= 0 || duration <= 0) {
+            revert ERR_CANNOT_BE_ZERO();
+        }
+
+        rewardsEndTime = block.timestamp + duration;
+        rewardsRate = _amount / duration;
+        lastUpdateTime = block.timestamp;
+        rewardsStartTime = block.timestamp;
+
+   }
+    
 
 
+    /**
+     * @notice The reason why we have "pending rewards" because it will allow users to withdraw pending rewards if the contract is not dynamic or pausable
+     * @dev Function to handle rewards
+     * @param _amount amount of stake tokens
+     */
+    function rewardsHandler(stakerInfo storage stakerInfo) internal returns(uint256 totalPendingRewards) {
+        // Check if the the the contract has staked tokens
+        if(totalStakedTokens > 0) {
+            // Create a function to calculate the user pending rewards and new rewards per tokens since the last time the user checked.
+            (uint256 _totalPendingReward, uint256 newRewardsPerToken) = calculateRewards(stakerInfo, msg.sender);
 
+            // Update the total rewards tokens for msg.sender
+            totalPendingRewards = _totalPendingReward;
 
+            // Update the new rewards per tokens
+            rewardsPerToken = newRewardsPerToken;
 
-
-    // The reason why we have "pending rewards" because it will allow users to withdraw pending rewards if the contract is not dynamic or pausable
-/// @dev Calculates the rewards and handles them (updating states and transfering them if "autocompound" is enabled)
-    /// @param staker The staker info
-    /// @return pendingReward The pending rewards
-    function _handleRewards(
-        StakerInfo storage staker
-    ) internal returns (uint256 pendingReward) {
-        /// @dev We need this check here because if totalStaked is 0 and the first ever staker wants to stake,
-        /// @dev calculation will fail because the formula includes division by totalStaked
-        if (totalStaked > 0) {
-            
-            // @audit Calculating users pending "rewards" and the "newRewardsPerToken"
-            (
-                uint256 _pendingReward,
-                uint256 newRewardPerToken
-            ) = _calculatePendingRewards(staker, msg.sender);
-
-            // @audit Updating users "pending rewards" and the "newRewardsPerToken"
-            pendingReward = _pendingReward;
-            rewardPerToken = newRewardPerToken;
-
-            // Updating the paid rewards paid for users
-            userRewardPerTokenPaid[msg.sender] = rewardPerToken;
+            userRewardsPerTokensPaid[msg.sender] = rewardsPerToken; 
         }
 
         lastUpdateTime = _lastApplicableTime();
 
-        staker.rewardDebt += pendingReward;
-        staker.lastRewardTimestamp = block.timestamp;
+        // Update staker info
+        stakerInfo.amountStaked += totalPendingRewards;
+        stakerInfo.lastTimestamp += block.timestamp;
     }
 
 
 
-    /// @dev calculating the pending rewards and the new reward per token if the contract is dynamic
-    /// @param stakerInfo The staker info
-    /// @param _staker The address of the staker
-    /// @return totalReward The pending rewards
-    /// @return newRewardPerToken The pending rewards
-    function _calculatePendingRewards(
-        StakerInfo storage stakerInfo,
-        address _staker
-    ) internal view returns (uint256 totalReward, uint256 newRewardPerToken) {
-        
-        if (totalStaked > 0) {
+
+    function calculateRewards(StakerInfo storage stakerInfo, address _staker) 
+                              internal returns(uint256 totalPendingRewards, uint256 newRewardsPerToken) {
+
+            if( totalStakerTokens > 0) {
+
             /*** @notice - (rewardRate * (_lastApplicableTime() - lastUpdateTime)): Calculate how much rewards has been accumulated since
                             the last applicable time.
                          - * 1 ether is to add more zero (18 decimals)
                          - / totalStaked gives the per-tokens reward
                          - rewardsPerTokens + "adding the newly calculated per-token rewards" to the existing per token rewards;        
-             */
+            */  
             // How much new reward is earn per tokens since our "last Applicable" "time" & "rate update"
             // or How much more rewards each token should get
-            newRewardPerToken = rewardPerToken + ((rewardRate * (_lastApplicableTime() - lastUpdateTime)) * 1 ether) / totalStaked;
-        }
+            newRewardsPerToken = rewardsPerToken + (( rewardRate * (_lastApplicableTime() - lastUpdateTime)) * 1 ether) / totalStakedTokens;
 
-        /***
-           *  - newRewardPerToken - userRewardPerTokenPaid[_staker]: how much reward per token Bob has earned since the last time he checked.
-           *  - stakerInfo.stakedAmount * : Multiplties Bob's staked tokens by the newRewardPerToken to find out Bob's total new rewards.
-           *  - divide it by 1 ether to back down to normal number because we multiple above by 1 ether
-        */
 
-        // How much new rewards Bob has earned on his staked since last time he checked (rewards rate, last timeStamp)
-        // Or how much new rewards Bob has earned.
-        totalReward =
-            (stakerInfo.stakedAmount *
-                (newRewardPerToken - userRewardPerTokenPaid[_staker])) /
-            1 ether;
+            /***
+            *  - newRewardPerToken - userRewardPerTokenPaid[_staker]: How much reward per token Bob has earned since the last time he checked.
+            *  - stakerInfo.stakedAmount * : Multiplties Bob's staked tokens by the newRewardPerToken to find out Bob's total new rewards.
+            *  - divide it by 1 ether to back down to normal number because we multiple above by 1 ether
+            */
+
+            // How much new rewards Bob has earned on his staked since last time he checked (rewards rate, last timeStamp)
+            // Or how much new rewards Bob has earned.
+            totalPendingRewards = (stakerInfo.stakedAmount * (newRewardPerToken - userRewardPerTokenPaid[msg.sender])) * 1 ether;
+
+            }                                                               
+
     }
 
 
-    /// @dev When calculating rewards, we want to don't want to include the current timestamp
-    /// @dev in the calculations if the contract isn't giving out rewards anymore.
-    /// @dev Returns the last applicable time based on the current time and the finish time of giving rewards.
-    /// @return The last applicable time.
-    function _lastApplicableTime() internal view returns (uint) {
-        return Math.min(block.timestamp, rewardFinishAt);
+    function _lastApplicableTime() returns view internal(uint256) {
+        Math.min(block.timestamp, rewardsEndTime);
     }
+
